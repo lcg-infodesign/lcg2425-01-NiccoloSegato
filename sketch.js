f973e3ee634209318e93bbab7cb54f2e51829a08
niccolosegato <segato117@gmail.com>
2024-10-16 15:43:26 +0200

    Consegna dell'assignment

--- sketch.js
+++ sketch.js
@@ -1,17 +1,49 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  background("#E7E7D2");
+
+  text('"Spectrum Colors Arranged by Chance I to VIII" - Kelly Ellsworth', 15, windowHeight - 15);
+  
+  let horProbability = 0.0;
+  let verProbability = 0.0;
+  
+  let gridSize = 20; // Size of each square
+  let rows = Math.floor(windowHeight / gridSize);
+  let cols = Math.floor(windowWidth / gridSize);
+
+  for (let row = 2; row < rows; row++) {
+    horProbability = 0;
+    for (let col = 2; col < cols; col++) {
+      if (random(1) < horProbability && random(1) < verProbability) {
+
+        // Randomly pick a color from the palette
+        let fillColor = random(["#9D362D", "#F5D863", "#51A8CD", "#8CAD65", "#774267", "#D98C46", "#D7A9A9", "#000000", "#3D6165"]);
+        fill(fillColor);
+
+        let x = col * gridSize;
+        let y = row * gridSize;
+
+        // Draw the square
+        rect(x, y, gridSize, gridSize);
+      }
+
+      // Make the probability of drawing a square increase as we move to the right and decrease after the middle
+      if (col < cols / 2) {
+        horProbability += 0.05;
+      } else {
+        horProbability -= 0.05;
+      }
+    }
+
+    // Make the probability of drawing a square increase as we move down and decrease after the middle
+    if (row < rows / 2) {
+      verProbability += 0.07;
+    } else {
+      verProbability -= 0.07;
+    }
+  }
 }
